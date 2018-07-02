e6f90731e01c55f775f2fbe163914c74179e9e15
Ja Shia <ja.shia@revelsystems.com>
2018-07-02 15:28:47 -0700

    makes some form changes

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ jQuery(document).ready(function($) {
 	$('#first_level_support').change(function() {
 		if ($(this).val() === "Yes") {
 			$('#number_of_agents').show();
+			$('#number_of_agents').prop('required',true);
+
 		} else {
 			$('#number_of_agents').hide();
+			$('#number_of_agents').removeAttr('required');
 		}
 	});
 	$('#channel-form').validate();
@@ -22,13 +25,20 @@ jQuery(document).ready(function($) {
 			MktoForms2.loadForm("//app-sj14.marketo.com", "804-YHP-876", 2583, function(form){
 				var x = $('input, select');
 				var	y = {};
-				for (var i = x.length - 1; i >= 0; i--) {
+				var notesValues,
+				notesNameArray = ['affiliated_with_tablet_pos', 'other_pos_companies', 'products', 'first_level_support'];
+				notesValues = {};
+				for ( var i = x.length - 1; i >= 0; i-- ) {
 					if (x[i].name !== 'channel-submit-button'){
 						y[x[i].name] = x[i].value; 
                     }
 				}
-				var theForm = form.vals(y);
-				console.log(theForm);
+				for ( var b = 0; b < notesNameArray.length; b++ ) {
+					notesValues.notesNameArray[b] = y[notesNameArray[b]];
+				}
+				//form.vals(y);
+				// form.submit();
+				console.log(notesValues);
 			});
 		}
 	});
